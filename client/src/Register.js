16ba2101b3be68647a845e828b3531cf76acd49c
Hemvyas <hem.vyas09@gmail.com>
2024-01-01 19:19:27 +0530

    register functionality handled with validation

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import axios from 'axios'
+import React,{ useState} from 'react'
 import styled from "styled-components"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {useNavigate} from "react-router-dom"
 
 const Container=styled.div`
 width:100vw;
 height:100vh;
 background:linear-gradient(rgba(255,255,255,0.5),rgba(255,255,255,0.5)),url("https://c4.wallpaperflare.com/wallpaper/253/638/452/light-background-hd-wallpaper-thumb.jpg")center;
-
 background-size:cover;
 display:flex;
 justify-content:center;
@@ -44,24 +47,93 @@ color:#fff;
 cursor:pointer;
 `
 const Register = () => {
+  const [username,setUsername]=useState("");
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  const [confirmPassword,setConfirmPassword]=useState("");
+  const navigate=useNavigate()
+
+  const toastOptions={
+    position:"top-center",
+    autoClose:5000,
+    hideProgressBar:false,
+    closeOnClick:true,
+    pauseOnHover:true,
+    draggable:true,
+    theme:"dark"
+   }
+
+   const hadleValidation=()=>{
+    if(email==="" || password==="" || confirmPassword==="" || username===""){
+      toast.error("Please fill all fields!",toastOptions);
+      return false;
+    }
+    else if(password!==confirmPassword){
+      toast.error("Passwords do not match!",toastOptions)
+      return false;
+    }
+    else if(username.length<3){
+      toast.error("Username should be at least 3 characters long!",toastOptions)
+      return false;
+    }
+    else if(password.length<8){
+      toast.error("Password should be at least 8 characters long!",toastOptions)
+      return false;
+    }
+      return true;
+   }
+  
+
+  const handleRegister=async(e)=>{
+    e.preventDefault();
+    if(hadleValidation()){
+      const res=await axios.post("http://localhost:5000/api/auth/register",{
+      username,
+      confirmPassword,
+      email,
+      password
+      });
+      if(res.status===501){
+      toast.error(res.msg,toastOptions);
+    }
+    if(res.status===201){
+    toast.success("User Registered Successfully!",toastOptions);
+  }
+  navigate('/login');
+}
+}
   return (
     <Container>
         <Wrapper>
             <Title>CREATE AN ACCOUNT</Title>
             <FORM>
-                <Input placeholder ="name"/>
-                <Input placeholder ="lastName"/>
-                <Input placeholder ="username"/>
-                <Input placeholder ="email"/>
-                <Input placeholder ="password"/>
-                <Input placeholder ="confirmPassword"/>
-                
+                <Input placeholder ="username" 
+                type='text'
+                value={username} 
+                onChange={(e)=>setUsername(e.target.value)}
+                />
+                <Input placeholder ="email" 
+                type='email'
+                value={email} 
+                onChange={(e)=>setEmail(e.target.value)}
+                />
+                <Input placeholder ="password" 
+                type="password"
+                value={password} 
+                onChange={(e)=>setPassword(e.target.value)}
+                />
+                <Input placeholder ="confirmPassword" 
+                type="password"
+                value={confirmPassword} 
+                onChange={(e)=>setConfirmPassword(e.target.value)}
+                />
                 <Agreement>
                     BY creating an account, I consent to the processing of my personal data in accordanace with the <b>PRIVACY POLICY</b>
                 </Agreement>
-                <BUTTON>REGISTER</BUTTON>
+                <BUTTON onClick={handleRegister}>REGISTER</BUTTON>
             </FORM>
         </Wrapper>
+        <ToastContainer/>
     </Container>
   )
 }
