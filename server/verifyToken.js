622ffa71cbdcc9817f620c834dbb92dadf07f412
Hemvyas <hem.vyas09@gmail.com>
2024-06-27 21:55:33 +0530

    .

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -2,26 +2,18 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = async (req, res, next) => {
   const token = req.cookies.token;
-  console.log("Token from cookie:", token); // Log the token
 
   if (!token) {
     return res.status(401).send({ auth: false, message: "No token provided" });
   }
 
   try {
-    jwt.verify(token, process.env.SECRET, (error, user) => {
-      if (error) {
-        return res.status(500).send({ auth: false, message: "Invalid Token" });
-      } else {
-        req.user = user;
-        next();
-      }
-    });
-  } catch (ex) {
-    console.log(ex);
-    res
-      .status(400)
-      .send({ auth: false, message: "Failed to authenticate token." });
+    const verified = jwt.verify(token, process.env.SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    console.error("Token verification error:", error);
+    res.status(400).json({ message: "Invalid token" });
   }
 };
 
